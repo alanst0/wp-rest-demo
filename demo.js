97293e187bd18bc61c274206122ba8ace27025a1
Alan Stahl <alanst@umich.edu>
2016-07-22 15:07:31 -0400

    Display profiles with titles

---
+++ demo.js
@@ -0,0 +1,45 @@
+function generateProfile(data) {
+    var panel = $('#profile-template').clone().attr('id', '');
+    panel.find('.panel-title').text(data.title.rendered);
+    return panel;
+}
+
+$(function() {
+    var id = 0;
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        var url = $('#url').val();
+        if (!url)
+            return;
+        id++;
+        $('<li><a href="#site' + id + '" data-toggle="tab"><button class="close" type="button">&times;</button><span>Loading...</span></a></li>').appendTo($('#tabs'));
+        var tabContent = $('<div class="tab-pane" id="site' + id + '"><span class="loading">Loading</span></div>').appendTo($('.tab-content'));
+        var tabLink = $("a[href='#site" + id + "']").tab('show');
+
+        $.getJSON(url + '/wp-json', function(data) {
+            tabLink.find('span').text(data.name);
+        }).fail(function() {
+            tabLink.find('span').text('Unknown');
+            tabContent.prepend('<div class="alert alert-warning">Could not retrieve site information<a href="#" class="close" data-dismiss="alert">&times;</a></div>');
+        });
+
+        $.getJSON(url + '/wp-json/wp/v2/profile', function(data) {
+            data.forEach(function(profile) {
+                tabContent.append(generateProfile(profile));
+            });
+        }).fail(function() {
+            tabContent.prepend('<div class="alert alert-danger">Could not retrieve profiles<a href="#" class="close" data-dismiss="alert">&times;</a></div>');
+        }).complete(function() {
+            tabContent.find('.loading').remove();
+        });
+    });
+
+    $('#tabs').on('click', 'button.close', function(e) {
+        e.preventDefault();
+        $('#main-tab').tab('show');
+        $($(this).parent('a').attr('href')).remove();
+        $(this).parent('a').remove();
+    });
+
+});
